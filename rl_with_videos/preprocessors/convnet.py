27d1cb6b9649b6d0c547f80e5f3b6bb132067721
Iroh Cao <40520605+IrohXu@users.noreply.github.com>
2021-11-21 00:55:37 -0500

    Upload convnet

--- rl_with_videos/preprocessors/convnet.py
+++ rl_with_videos/preprocessors/convnet.py
@@ -79,3 +79,67 @@ def convnet_preprocessor(
 
     model = PicklableKerasModel(inputs, output, name=name)
     return model
+
+
+def convnet_preprocessor_softmax(
+        input_shapes,
+        image_shape,
+        output_size,
+        conv_filters=(32, 32),
+        conv_kernel_sizes=((5, 5), (5, 5)),
+        pool_type='MaxPool2D',
+        pool_sizes=((2, 2), (2, 2)),
+        pool_strides=(2, 2),
+        dense_hidden_layer_sizes=(64, 64),
+        data_format='channels_last',
+        name="convnet_preprocessor_softmax",
+        make_picklable=True,
+        *args,
+        **kwargs):
+    
+    """
+    Just use it to test output
+    """
+    
+    if data_format == 'channels_last':
+        H, W, C = image_shape
+    elif data_format == 'channels_first':
+        C, H, W = image_shape
+
+    inputs = [
+        tf.keras.layers.Input(shape=input_shape)
+        for input_shape in input_shapes
+    ]
+
+
+    concatenated_input = tf.keras.layers.Lambda(
+        lambda x: tf.concat(x, axis=-1)
+    )(inputs)
+
+    images_flat, input_raw = tf.keras.layers.Lambda(
+        lambda x: [x[..., :H * W * C], x[..., H * W * C:]]
+    )(concatenated_input)
+
+    images = tf.keras.layers.Reshape(image_shape)(images_flat)
+    conv_out = conv_layers(images, conv_filters, conv_kernel_sizes, pool_sizes, pool_strides, pool_type, *args, **kwargs)
+
+    flattened = tf.keras.layers.Flatten()(conv_out)
+    concatenated_output = tf.keras.layers.Lambda(
+        lambda x: tf.concat(x, axis=-1)
+    )([flattened, input_raw])
+
+    output = (
+        feedforward_model(
+            input_shapes=(concatenated_output.shape[1:].as_list(), ),
+            output_size=output_size,
+            hidden_layer_sizes=dense_hidden_layer_sizes,
+            activation='relu',
+            output_activation='softmax',
+            *args,
+            **kwargs
+        )([concatenated_output])
+        if dense_hidden_layer_sizes
+        else concatenated_output)
+
+    model = PicklableKerasModel(inputs, output, name=name)
+    return model
